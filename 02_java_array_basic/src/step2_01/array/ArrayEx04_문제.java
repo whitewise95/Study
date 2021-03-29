e5febd4a8aab14e1e7a4fc0c752cbd6594cc7ca4
user <user@401-14>
2021-03-29 17:34:39 +0900

    3/29 17:34

---
+++ 02_java_array_basic/src/step2_01/array/ArrayEx04_문제.java
@@ -0,0 +1,57 @@
+package step2_01.array;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+ * # 학생성적관리 프로그램[1단계] : 학생점수
+ */
+
+
+public class ArrayEx04_문제 {
+
+	public static void main(String[] args) {
+		Random ran = new Random();
+		int[] arr = new int[5];
+
+		// 문제1) arr배열에 1~100점 사이의 랜덤 정수를 5개 저장
+		// 예   1) 87, 11, 92, 14, 47
+		for(int i =0; i<arr.length; i++) {
+			int ran1 = ran.nextInt(100)+1;
+		    arr[i]= ran1; 
+		}
+		System.out.println(Arrays.toString(arr)); 
+		
+		
+		
+		// 문제2) 전교생의 총점과 평균 출력
+		// 예   2) 총점(251) 평균(50.2)
+		int sum = 0;
+		for(int i =0; i<arr.length; i++) {
+			int ran1 = ran.nextInt(100)+1;
+		    arr[i]= ran1; 
+		    sum+=arr[i];
+		}
+		System.out.println("총점:"+sum);
+		System.out.println("평균:"+sum/5);
+		
+		
+		
+		
+		
+		
+		// 문제3) 성적이 60점 이상이면 합격. 합격생 수 출력
+		// 예   3) 2명
+		int sum1 = 0;
+		for(int i =0; i<arr.length; i++) {
+			int ran1 = ran.nextInt(100)+1;
+		    arr[i]= ran1; 
+		    if(arr[i]>=60) {
+		    	
+		    }
+		}
+		
+
+	}
+	
+}
