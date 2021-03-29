4b73749b83d0431f588780d58de517a843df9fa5
coffe <coffe@DESKTOP-DNP38AM>
2021-03-30 02:39:58 +0900

    3/30 2:39

--- 02_java_array_basic/src/step2_01/array/ArrayEx05_문제.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx05_문제.java
@@ -11,47 +11,68 @@ public class ArrayEx05_문제 {
 	public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 	
-		// 인덱스    0   1   2   3   4
-		int[] arr = {87, 11, 45, 98, 23};
-		
-		// 문제1) 인덱스를 입력받아 성적 출력
-		// 정답1) 인덱스 입력 : 1	성적 : 11점
-		System.out.println("인덱스를 입력해주세여");
-		int number = scan.nextInt();
-		if(number == 0) System.out.println(arr[0]);
-		if(number == 1) System.out.println(arr[1]);
-		if(number == 2) System.out.println(arr[2]);
-		if(number == 3) System.out.println(arr[3]);
-		if(number == 4) System.out.println(arr[4]);
-		
-		
-	
-		
-		
-		// 문제2) 성적을 입력받아 인덱스 출력
-		// 정답2) 성적 입력 : 11		인덱스 : 1
-		
-		System.out.println("성적를 입력해주세여");
-		int number1 = scan.nextInt();
-		if(number1 == 0) System.out.println(arr[0]);
-		if(number1 == 1) System.out.println(arr[1]);
-		if(number1 == 2) System.out.println(arr[2]);
-		if(number1 == 3) System.out.println(arr[3]);
-		if(number1 == 4) System.out.println(arr[4]);
-		
-		
-		
-		
-		
-		
-		
-		
+//		// 인덱스      0   1   2   3   4
+//		int[] arr = {87, 11, 45, 98, 23};
+//		
+//		// 문제1) 인덱스를 입력받아 성적 출력
+//		// 정답1) 인덱스 입력 : 1	성적 : 11점
+//		System.out.println("인덱스를 입력해주세여");
+//		int number = scan.nextInt();
+//		if(number == 0) System.out.println(arr[0]);
+//		if(number == 1) System.out.println(arr[1]);
+//		if(number == 2) System.out.println(arr[2]);
+//		if(number == 3) System.out.println(arr[3]);
+//		if(number == 4) System.out.println(arr[4]);
+//		
+//		
+//	
+//		
+//		
+//		// 문제2) 성적을 입력받아 인덱스 출력
+//		// 정답2) 성적 입력 : 11		인덱스 : 1
+//		
+//		System.out.println("성적를 입력해주세여");
+//		int score = scan.nextInt();
+//		for(int i=0; i<5; i++) {
+//			if(arr[i]==score) {
+//				System.out.println("인덱스"+i);
+//			}
+//		}
+//		
+//		
+//		
+//		
+//		
+//		
+//		
 		
 		int[] hakbuns = {1001, 1002, 1003, 1004, 1005};
 		int[] scores  = {  87,   11,   45,   98,   23};
 		
 		// 문제3) 학번을 입력받아 성적 출력
 		// 정답3) 학번 입력 : 1003	성적 : 45점
+		
+		System.out.println("학번을 입력해주세요");
+		int hakbun = scan.nextInt();
+	    for(int i=0; i<5; i++) {
+	    	if(hakbuns[i]==hakbun) {
+	    		System.out.println(scores[i]);
+	    		
+	    	}
+	    }
+		
+		
+		
+		
+		
+	    
+		
+		
+		
+		
+		
+		
+		
 
 	}
 	
--- 02_java_array_basic/src/step2_01/array/ArrayEx06_문제.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx06_문제.java
@@ -1,5 +1,7 @@
 package step2_01.array;
 
+import java.util.Scanner;
+
 /*
  * # 학생성적관리 프로그램[3단계] : 예외처리
  */
@@ -9,15 +11,25 @@ package step2_01.array;
 public class ArrayEx06_문제 {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
 		int[] hakbuns = {1001, 1002, 1003, 1004, 1005};
 		int[] scores  = {  87,   11,   45,   98,   23};
 		
+	
 		// 문제) 학번을 입력받아 성적 출력
 		// 		단, 없는학번 입력 시 예외처리
 		// 예)
 		// 학번 입력 : 1002		성적 : 11점
 		// 학번 입력 : 1000		해당학번은 존재하지 않습니다.
+		
+		System.out.println("학번을 입력해주세요");
+		int hakbun = scan.nextInt();
+		if(hakbun==hakbuns.length) {
+			for(int i=0; i<5; i++) {
+				
+			}
+		}
+		
 		
 	}
 	
