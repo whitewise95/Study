1f7e0209eccb123a21cc56c15c96916ce3e7e0d1
user <user@401-14>
2021-04-28 17:46:44 +0900

    4/28 17:46

--- 07_java_class_array/src/step7_01/classArray/ClassArrayEx12_문제.java
+++ 07_java_class_array/src/step7_01/classArray/ClassArrayEx12_문제.java
@@ -1,7 +1,7 @@
 package step7_01.classArray;
 
 // 문자열(2단계) [문제]
-/*
+
 class Test{
 
 	int num;	  // 인덱스
@@ -16,7 +16,6 @@ class Test{
 	}
 	
 }
-*/
 
 public class ClassArrayEx12_문제 {
 
@@ -28,18 +27,27 @@ public class ClassArrayEx12_문제 {
 		// 두번째 /는 데이터의 개수이다.
 		// 두번째 / 이후부터 data의 정보이다.
 		
-		// Test[] t;
-		String data ="6\n";						// Test의 크기
-		data += "0/2/a/b\n";					// 인덱스 / 데이터개수 / 데이터 ~~
-		data += "1/3/a/b/c\n";					// 인덱스 / 데이터개수 / 데이터 ~~
-		data += "2/5/a/b/c/d/e\n";				// 인덱스 / 데이터개수 / 데이터 ~~
-		data += "3/4/a/b/c/d\n";				// 인덱스 / 데이터개수 / 데이터 ~~
-		data += "4/3/a/b/c\n";					// 인덱스 / 데이터개수 / 데이터 ~~
-		data += "5/1/a";						// 인덱스 / 데이터개수 / 데이터 ~~
+		
+		String data ="6\n";						// 0Test의 크기
+		data += "0/2/a/b\n";					// 1인덱스 / 데이터개수 / 데이터 ~~
+		data += "1/3/a/b/c\n";					// 2인덱스 / 데이터개수 / 데이터 ~~
+		data += "2/5/a/b/c/d/e\n";				// 3인덱스 / 데이터개수 / 데이터 ~~
+		data += "3/4/a/b/c/d\n";				// 4인덱스 / 데이터개수 / 데이터 ~~
+		data += "4/3/a/b/c\n";					// 5인덱스 / 데이터개수 / 데이터 ~~
+		data += "5/1/a";						// 6인덱스 / 데이터개수 / 데이터 ~~
 
 			
+		String temp[] =data.split("\n"); 
 		
-		
+		for (int i = 1; i < temp.length; i++) {
+			int size =Integer.parseInt(temp[0]);
+			Test[] t = new Test[size];
+			t[i-1] = new Test();
+			String temp2[] =temp[i].split("/");
+			t[i-1].num = temp2[0]
+			t[i-1].num = 
+			t[i-1].num = 
+		}
 
 	}
 
