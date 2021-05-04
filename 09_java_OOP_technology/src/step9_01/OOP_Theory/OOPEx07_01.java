4ad7c4c82acce0502a4a8114448545a697577463
user <user@401-14>
2021-05-04 17:47:44 +0900

    17:47 5/4

---
+++ 09_java_OOP_technology/src/step9_01/OOP_Theory/OOPEx07_01.java
@@ -0,0 +1,14 @@
+package step9_01.OOP_Theory;
+
+public interface OOPEx07_01 {
+
+	
+	 abstract void showData1();
+	 abstract void showData2();
+	 abstract void showData3();
+	 void showData4();
+	 void showData5();
+	 void showData6();
+	 void showData7();
+	 
+}
