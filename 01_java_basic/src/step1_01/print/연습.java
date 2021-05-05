47d173790ddef8efaf0ebdfdb5b26f2f28d2f198
coffe <coffe@DESKTOP-DNP38AM>
2021-05-06 00:33:31 +0900

    5/6 12:33

--- 01_java_basic/src/step1_01/print/연습.java
+++ 01_java_basic/src/step1_01/print/연습.java
@@ -1,18 +1,23 @@
 package step1_01.print;
 
-public class 연습 {
 
+class Test {
 	
-	 
-	    public static void main(String[] args) 
-	    {
-	        System.out.print("가나다라");
-	        System.out.println("마바사아");
-	        System.out.println("===========");
-	        System.out.println("가나다라");
-	        System.out.println("마바사아");
+	void test() {
+		
+		System.out.println("안녕하세요");
+	}
+	
+}
+public class 연습 {
+	    public static void main(String[] args) {
+	    	
+	    	
+	    	Test e = new Test();
+	    	
+	    	e.test();
+	        
 	    }
-	 
 	}
 
 
