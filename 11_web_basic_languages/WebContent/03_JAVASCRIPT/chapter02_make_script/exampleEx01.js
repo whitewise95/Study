e4e3fb7c4f49c6c7c516b9d8fcfaac168448d45c
user <user@401-14>
2021-05-26 17:45:04 +0900

    5/26

---
+++ 11_web_basic_languages/WebContent/03_JAVASCRIPT/chapter02_make_script/exampleEx01.js
@@ -0,0 +1,12 @@
+alert("example.js파일 입니다.")
+
+document.write("<h1>자바 스크립트의 외부 파일 입니다.</h1>");
+document.write("hello javascript");
+document.write("hello javascript1");
+document.write("hello javascript2");
+document.write("hello javascript3");
+
+
+console.log("hello javascript1");
+console.log("hello javascript2");
+console.log("hello javascript3");
