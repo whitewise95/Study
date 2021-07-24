f60c2c05b7494ecdcb41ef73efd62657ca027094
coffe <coffe@DESKTOP-DNP38AM>
2021-07-25 00:40:56 +0900

    7/25

--- 01_java_basic/src/step1_01/print/연습.java
+++ 01_java_basic/src/step1_01/print/연습.java
@@ -1,36 +1,3 @@
-package step1_01.print;
-
- class Test {
-	private int s ;
-	private int q ;
-	String[] w = new String[2];
-	
-	public void Test() {
-		s= 1;
-		q= 1;
-	}
-	
-	void s() {
-		w[s]="s";
-		
-	}
-	void q() {
-		System.out.println(w[q]);
-	}
-	
-	
-	
-}
-public class 연습 {
-	    public static void main(String[] args) {
-	    	
-	    	
-	    	Test e = new Test();
-	    	
-	    	e.s();
-	    	e.q();
-	    }
-	}
 
 
 //정보처리기사 산업기사 sqld 네트워크관리사 CCNA(국제)
